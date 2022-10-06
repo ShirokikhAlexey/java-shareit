2396a8920a682f1ffb96ceb767c569c9ec5a9563
ShirokihAlexey <shirokihaleksey@yandex.ru>
2022-10-06 23:46:20 +0300

    fix: правки

--- src/main/java/ru/practicum/shareit/db/db/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/db/db/BookingRepository.java
@@ -11,34 +11,34 @@ import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
-    @Query("select new ru.practicum.shareit.booking.BookingDto(b.booked_by_id, b.item_id, b.from_timestamp, b.to_timestamp, b.status, b.review) " +
+    @Query(value = "select new ru.practicum.shareit.booking.BookingDto(b.booked_by_id, b.item_id, b.from_timestamp, b.to_timestamp, b.status, b.review) " +
             "from Booking as b " +
             "where (upper(b.status) = upper('?2') or upper(?2) = 'ALL') " +
             "and b.booked_by_id = ?1 " +
-            "order by b.from_timestamp desc")
+            "order by b.from_timestamp desc", nativeQuery = true)
     List<BookingDto> getByStatus(Integer userId, String state);
 
-    @Query("select new ru.practicum.shareit.booking.BookingDto(b.booked_by_id, b.item_id, b.from_timestamp, b.to_timestamp, b.status, b.review) " +
+    @Query(value = "select new ru.practicum.shareit.booking.BookingDto(b.booked_by_id, b.item_id, b.from_timestamp, b.to_timestamp, b.status, b.review) " +
             "from Booking as b " +
             "join Item as i on i.id = b.item_id " +
             "where (upper(b.status) = upper('?2') or upper(?2) = 'ALL') " +
             "and i.owner_id = ?1 " +
-            "order by b.from_timestamp desc")
+            "order by b.from_timestamp desc", nativeQuery = true)
     List<BookingDto> getUserItemsBookings(Integer userId, String state);
 
-    @Query("select b.to_timestamp " +
+    @Query(value = "select LocalDateTime.parse(b.to_timestamp) " +
             "from Booking as b " +
             "where b.item_id = ?1 " +
             "and b.status = 'PAST' " +
             "order by b.from_timestamp desc " +
-            "limit 1")
+            "limit 1", nativeQuery = true)
     LocalDateTime getItemLatestBooking(Integer itemId);
 
-    @Query("select b.to_timestamp " +
+    @Query(value = "select LocalDateTime.parse(b.to_timestamp) " +
             "from Booking as b " +
             "where b.item_id = ?1 " +
             "and b.status = 'FUTURE' " +
             "order by b.from_timestamp asc " +
-            "limit 1")
+            "limit 1", nativeQuery = true)
     LocalDateTime getItemNearestBooking(Integer itemId);
 }
